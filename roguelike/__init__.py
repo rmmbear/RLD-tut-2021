f3ed73ba6e04a2878702d90e4a1ce436de1772e9
rmmbear <rmmbear@users.noreply.github.com>
2021-07-01 16:15:17 +0200

    Populate grid with some sprites, order drawing by groups

--- roguelike/__init__.py
+++ roguelike/__init__.py
@@ -1,12 +1,14 @@
 import os
 import enum
 import logging
+import dataclasses as dc
 from typing import Optional, Tuple
 
+import numpy
 import pyglet
 from pyglet.window import key
 
-LOG_FORMAT = logging.Formatter("%(asctime)s [%(levelname)s]: %(message)s", "%Y-%m-%d %H:%M:%S")
+LOG_FORMAT = logging.Formatter("%(asctime)s [%(levelname)s]: %(message)s")#, "%Y-%m-%d %H:%M:%S")
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 TH = logging.StreamHandler()
@@ -26,7 +28,9 @@ UPDATE_INTERVAL = 1/120 # 120HZ
 
 #TODO: constrain movement to grid
 #TODO: make the grid/sprite size dynamic/adjustable
+#TODO: make the grid scale automatically when resizing the window
 #TODO: make sprite color adjustable
+
 
 @enum.unique
 class Directions(enum.Enum):
@@ -57,11 +61,7 @@ KEY_TO_DIR = {
 
 
 class Player:
-    def __init__(self, batch: pyglet.graphics.Batch, x: int = 0, y: int = 0,
-                 sprite: Optional[pyglet.sprite.Sprite] = None) -> None:
-        if not sprite:
-            sprite = pyglet.sprite.Sprite(IMG_FONT[6, 0], batch=batch)
-
+    def __init__(self, sprite: pyglet.sprite.Sprite, x: int = 0, y: int = 0) -> None:
         self.sprite = sprite
         self.sprite.x = x
         self.sprite.y = y
@@ -91,41 +91,85 @@ class Player:
 # grid is the section of the map data currently loaded
 # view is the subset of the grid actually displayed
 
+@dc.dataclass
+class Cell:
+    x: int
+    y: int
+    sprite: Optional[pyglet.sprite.Sprite] = None
+    entities: list = dc.field(default_factory=list)
+
+
+    @property
+    def sprite(self):
+        return self._sprite
+
+    @sprite.setter
+    def sprite(self, sprite: pyglet.sprite.Sprite):
+        sprite.x = self.x
+        sprite.y = self.y
+        sprite.anchor_y = 0
+        sprite.anchor_x = 0
+        sprite.color = (2000,200,255)
+        self._sprite = sprite
+
 
 class Map:
     ...
 
+
+
 class Grid:
-    def __init__(self, window: pyglet.window.Window) -> None:
-        self.player = Player(batch=window.main_batch, x=window.width//2, y=window.height//2)
-        self.game_objects = [self.player]
+    def __init__(self, grid_columns: int, grid_rows: int, window: pyglet.window.Window) -> None:
+        csize = 20
+        self._grid = numpy.empty((grid_columns, grid_rows), dtype=Cell)
+        LOGGER.debug("Initializing grid")
+        for i in range(grid_columns * grid_rows):
+            row = i//grid_columns
+            column = i - (row*grid_columns)
+            x = csize * column
+            y = csize * row
+            sprite = pyglet.sprite.Sprite(
+                IMG_FONT[6, 11], batch=window.main_batch, group=window.grp_background)
+            self._grid[column][row] = Cell(x=x, y=y, sprite=sprite)
+
+        self._grid.reshape(grid_columns,grid_rows)
+        LOGGER.debug("Grid initialized")
+
+        player_sprite = pyglet.sprite.Sprite(
+            IMG_FONT[6, 0], batch=window.main_batch, group=window.grp_foreground)
+        self.player = Player(x=window.width//2, y=window.height//2, sprite=player_sprite)
         window.push_handlers(self.player)
+        self.game_objects = [self.player]
         pyglet.clock.schedule_interval(self.update, UPDATE_INTERVAL)
+
 
     def update(self, delta: float) -> None:
         for obj in self.game_objects:
             obj.update(delta)
 
 
-
 class GameWindow(pyglet.window.Window):
     def __init__(self):
         super().__init__(800, 600, resizable=True)
         self.main_batch = pyglet.graphics.Batch()
+        self.grp_background = pyglet.graphics.OrderedGroup(0)
+        self.grp_foreground = pyglet.graphics.OrderedGroup(1)
+        self.grp_interface = pyglet.graphics.OrderedGroup(2)
+
         self.label = pyglet.text.Label(
             text="Hello roguelike world",
             font_name="monogram",
             font_size=32,
             x=self.width//2, y=self.height-100,
             anchor_x="center", anchor_y="center",
-            batch=self.main_batch
+            batch=self.main_batch, group=self.grp_interface
         )
 
-        self.grid = Grid(self)
+        self.grid = Grid(40, 30, self)
 
 
     def on_draw(self) -> None:
-        self.clear()
+        #self.clear()
         self.main_batch.draw()
 
 
