5d86457d09d4cc293e8b5360540fb2a1c901544b
rmmbear <rmmbear@users.noreply.github.com>
2021-07-01 10:37:31 +0200

    Simplify logic

--- roguelike/__init__.py
+++ roguelike/__init__.py
@@ -6,7 +6,7 @@ from typing import Optional, Tuple
 import pyglet
 from pyglet.window import key
 
-LOG_FORMAT = logging.Formatter("%(asctime)s [%(levelname)s]: %(message)s")
+LOG_FORMAT = logging.Formatter("%(asctime)s [%(levelname)s]: %(message)s", "%Y-%m-%d %H:%M:%S")
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 TH = logging.StreamHandler()
@@ -17,8 +17,8 @@ LOGGER.addHandler(TH)
 # add resource folder to resources, register it as font directory
 DIR_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 DIR_RES = os.path.join(DIR_ROOT, "res")
-pyglet.resource.path = [DIR_RES]
 pyglet.font.add_directory(DIR_RES)
+pyglet.resource.path = [DIR_RES]
 pyglet.resource.reindex()
 
 IMG_FONT = pyglet.image.ImageGrid(pyglet.image.load("res/dejavu10x10_gs_tc.png"), 8, 32)
@@ -27,18 +27,35 @@ IMG_FONT = pyglet.image.ImageGrid(pyglet.image.load("res/dejavu10x10_gs_tc.png")
 #TODO: make the grid/sprite size dynamic/adjustable
 #TODO: make sprite color adjustable
 
+@enum.unique
+class Directions(enum.Enum):
+    LEFT = (-1, 0)
+    LEFT_UP = (-1, 1)
+    UP = (0, 1)
+    RIGHT_UP = (1, 1)
+    RIGHT = (1, 0)
+    RIGHT_DOWN = (1, -1)
+    DOWN = (0, -1)
+    LEFT_DOWN = (-1, -1)
+
+
+KEY_TO_DIR = {
+    key.LEFT : Directions.LEFT,
+    key.NUM_4: Directions.LEFT,
+    key.NUM_7: Directions.LEFT_UP,
+    key.UP   : Directions.UP,
+    key.NUM_8: Directions.UP,
+    key.NUM_9: Directions.RIGHT_UP,
+    key.RIGHT: Directions.RIGHT,
+    key.NUM_6: Directions.RIGHT,
+    key.NUM_3: Directions.RIGHT_DOWN,
+    key.DOWN : Directions.DOWN,
+    key.NUM_2: Directions.DOWN,
+    key.NUM_1: Directions.LEFT_DOWN,
+}
+
+
 class Player:
-    class Movement(enum.Enum):
-        LEFT = (-1, 0)
-        LEFT_UP = (-1, 1)
-        UP = (0, 1)
-        RIGHT_UP = (1, 1)
-        RIGHT = (1, 0)
-        RIGHT_DOWN = (1, -1)
-        DOWN = (0, -1)
-        LEFT_DOWN = (-1, -1)
-
-
     def __init__(self, batch: pyglet.graphics.Batch, x: int = 0, y: int = 0,
                  sprite: Optional[pyglet.sprite.Sprite] = None) -> None:
         if not sprite:
@@ -52,22 +69,9 @@ class Player:
 
 
     def on_key_press(self, symbol: int, modifiers: int) -> None:
-        if symbol in (key.LEFT, key.NUM_4):
-            self.movement = (*self.Movement.LEFT.value, symbol)
-        elif symbol == key.NUM_7:
-            self.movement = (*self.Movement.LEFT_UP.value, symbol)
-        elif symbol in (key.UP, key.NUM_8):
-            self.movement = (*self.Movement.UP.value, symbol)
-        elif symbol == key.NUM_9:
-            self.movement = (*self.Movement.RIGHT_UP.value, symbol)
-        elif symbol in (key.RIGHT, key.NUM_6):
-            self.movement = (*self.Movement.RIGHT.value, symbol)
-        elif symbol == key.NUM_3:
-            self.movement = (*self.Movement.RIGHT_DOWN.value, symbol)
-        elif symbol in (key.DOWN, key.NUM_2):
-            self.movement = (*self.Movement.DOWN.value, symbol)
-        elif symbol == key.NUM_1:
-            self.movement = (*self.Movement.LEFT_DOWN.value, symbol)
+        movement = KEY_TO_DIR.get(symbol)
+        if movement:
+            self.movement = (*movement.value, symbol)
 
 
     def on_key_release(self, symbol: int, modifiers: int) -> None:
